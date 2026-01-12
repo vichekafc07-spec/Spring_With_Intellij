0661069cb049244228a120cc97518ac711370561
Vichika <vichekafc07@gmail.com>
2026-01-12 20:59:01 +0700

    Junit test

---
+++ src/test/java/com/library_management/Test.java
@@ -0,0 +1,11 @@
+package com.library_management;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test {
+    @org.junit.jupiter.api.Test
+    void testAddition() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}
